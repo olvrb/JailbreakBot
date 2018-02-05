87fd9f0c2d3fca5512ce9295dcaf39e57723d842
Oliver Boudet <oboudet1@me.com>
2018-02-05 21:06:57 +0100

    pirate command should be working

--- commands/roles/pirate.js
+++ commands/roles/pirate.js
@@ -25,22 +25,22 @@ module.exports = class ReplyCommand extends Command {
         });
     }
     hasPermission(message) {
-        return (message.member.roles.exists("name", "Geniuses™") || message.member.roles.exists("name", "Moderators")) && message.guild;
+        return (message.member.roles.exists("name", "Geniuses™") || message.member.roles.exists("name", "Moderators"));
     }
     async run(message, { member, reason }) {
         message.delete();
-        if (member.roles.exists("name", "Geniuses™")) return message.reply("You can't give a genius the pirate role!");
-        const preCheck = await db.fetchObject(member.user.id + "_pirate");
-        const pirateReports = message.guild.channels.find("name", "bot-testing");
+        //if (member.roles.exists("name", "Geniuses™")) return message.reply("You can't give a genius the pirate role!");
+        const preCheck = await db.fetchObject(message.guild.id + member.user.id + "_pirate");
+        const pirateReports = message.guild.channels.find("name", "pirate-reports");
         const pirateRole = message.guild.roles.find("name", "Pirate");
         if (!reason || preCheck.text === "not_pirate"){
             member.roles.remove(member.roles.find("name", "Pirate"));
-            const data = await db.fetchObject(member.user.id + "_pirate");
+            const data = await db.fetchObject(message.guild.id + member.user.id + "_pirate");
+            console.log(data);
             const pirateMessage = await pirateReports.messages.fetch(data.text);
             pirateMessage.delete();
-            db.updateText(member.user.id + "_pirate", "not_pirate");
-            return message.reply(`removed role from ${member}`);
-
+            db.updateText(message.guild.id + member.user.id + "_pirate");
+            return message.reply(`removed role from ${member}.`);
         }
         const roleArray = member.roles.array();
         roleArray.push(pirateRole);
@@ -54,8 +54,8 @@ module.exports = class ReplyCommand extends Command {
             .setDescription(`${member.user.username} is a pirate.`)
             .addField("Reason", reason)
             .setColor("RANDOM")
+            .setFooter(`Done by ${message.author.username}#${message.author.tag}`, message.author.displayAvatarURL())
         const m = await pirateReports.send(embed);
-        db.updateText(member.user.id + "_pirate", m.id);
-
+        db.updateText(message.guild.id + member.user.id + "_pirate", m.id);
     }
 };
