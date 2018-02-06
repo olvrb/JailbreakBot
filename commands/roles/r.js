61f388768b12b84ea8f3a48a434689f46a2a96dd
Oliver Boudet <oboudet1@me.com>
2018-02-06 14:11:31 +0100

    no longer deletes message in r command

--- commands/roles/r.js
+++ commands/roles/r.js
@@ -1,5 +1,9 @@
-const { Command } = require("discord.js-commando");
-const { MessageEmbed } = require("discord.js");
+const {
+    Command
+} = require("discord.js-commando");
+const {
+    MessageEmbed
+} = require("discord.js");
 const db = require("quick.db");
 module.exports = class ReplyCommand extends Command {
     constructor(client) {
@@ -10,29 +14,27 @@ module.exports = class ReplyCommand extends Command {
             memberName: 'r',
             description: 'Get or remove a role from the available list.',
             examples: ['r LiberiOS'],
-            args: [
-                {
-                    key: "role",
-                    prompt: "Please specify a role.",
-                    type: "string"
-                }
-            ]
+            args: [{
+                key: "role",
+                prompt: "Please specify a role.",
+                type: "string"
+            }]
         });
     }
     hasPermission(message) {
         return message.guild;
     }
-    async run(message, { role }) {
+    async run(message, {
+        role
+    }) {
         const roles = ["iOS", "macOS", "tvOS", "wathcOS", "Other Updates", "Electra", "Meridian", "g0blin", "LiberiOS", "h3lix", "overcl0ck"];
-        if (!(roles.indexOf(role) > -1)) return message.reply("!R That role is invalid.").then(e => e.delete(5000));
+        if (!(roles.indexOf(role) > -1)) return message.author.send("That role is invalid.");
         const newRole = message.guild.roles.find("name", role);
         //const roleCollection = message.member.roles.array();
-        setTimeout(() => {
-            if (message.member.roles.exists("name", role)) {
-                message.member.roles.remove(newRole);
-                return message.author.send(`!R Removed \`${newRole.name}\`.`).then(e => e.delete(5000));
-            }
-        }, 15e2);
-        
+        if (message.member.roles.exists("name", role)) {
+            message.member.roles.remove(newRole);
+            return message.author.send(`Removed \`${newRole.name}\`.`).then(e => e.delete(1e4));
+        }
+
     }
 };
